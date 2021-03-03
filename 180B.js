a5ca1c068d96f83ebc1ab0954876270d65713814
Arely Vasquez <47491918+arv020@users.noreply.github.com>
2021-03-02 17:00:39 -0800

    Update 180B.js

--- 180B.js
+++ 180B.js
@@ -313,7 +313,11 @@ function downloadappear() {
     }
 }
 
+const checkboxes = document.getElementsByClassName('task-list-item-checkbox');
 
+Array.prototype.forEach.call(checkboxes, function (e) {
+  e.removeAttribute('disabled');
+});
 
 
 function init() { 
